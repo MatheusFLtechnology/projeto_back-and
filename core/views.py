3deaeb623712525820005e65b6189ade5b3f2729
Matheus <matheus.technology988@gmail.com>
2022-06-26 15:33:55 -0300

    adicionei elementos na pagina index.html

--- core/views.py
+++ core/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+ 
+def home(request):
+    return render(request, 'index.html',{'nome':'IFRN'})
 
 
 def olamundo(request):
    return HttpResponse("<h1>ola mundo</h1>")
+
+   
 
 def cadastro(request):
    return HttpResponse("cadastro")    
